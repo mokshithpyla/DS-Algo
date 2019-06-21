fa937463df8327cb7df4e8569210f46dd1868fa3
Mokshith Pyla <mokshithpyla@gmail.com>
2019-06-21 16:43:58 +0530

    Bottom-up Fibonacci DP

---
+++ bottom_up_fib.py
@@ -0,0 +1,24 @@
+
+# def fib(n):
+#     if n==1 or n==2:
+#         return 1
+#     bottom_up=[None]*(n+1)
+#     bottom_up[1]=1
+#     bottom_up[2]=2
+#     for i in range(3,n+1):
+#         bottom_up[i]=bottom_up[i-1]+bottom_up[i-2]
+#     return bottom_up[n]
+#
+# n=int(input('Enter value of n:'))
+# print(n,'th fibonacci term is: ',fib(n),sep='')
+
+fib={}
+f=0
+n=int(input("Enter the value of n: "))
+for i in range(1,n+1):
+    if i <= 2:
+        f = 1
+    else:
+        f=fib[i-1] + fib[i-2]
+    fib[i] = f
+print(fib[n])
