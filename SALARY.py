efebf3901f4155c8c6fbbd81653f145432cf0efb
Mokshith Pyla <mokshithpyla@gmail.com>
2019-06-22 13:35:26 +0530

    added file

---
+++ SALARY.py
@@ -0,0 +1,11 @@
+# https://www.codechef.com/submit/SALARY
+# Hint: Increasing salary of only winners is same as decreasing for just the loser
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    s=list(map(int,input().split()))
+    turns = 0
+    s.sort()
+    for i in range(n):
+        turns+=s[i]-s[0]
+    print(turns)
