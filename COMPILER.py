8fef863a8669bd5d1d9eedb709ff58ed259b462b
Mokshith Pyla <mokshithpyla@gmail.com>
2019-07-04 13:47:54 +0530

    stack solution added

---
+++ COMPILER.py
@@ -0,0 +1,21 @@
+t = int(input())
+for _ in range(t):
+    S = list(input())
+    t = 0
+    ans = 0
+    for i in range(len(S)):
+        if S[i] == '<':
+            t += 1
+        else:
+            t -= 1
+            if t == 0:
+                ans = max(ans, i+1)
+            elif t < 0:
+                break   # string s whole is invalid.
+
+    print(ans)
+
+# 3
+# <<>>
+# ><
+# <>>>
