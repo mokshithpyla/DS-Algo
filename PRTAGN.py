47f670551bd1e8408c9cd76861394fbae66140a2
Mokshith Pyla <mokshithpyla@gmail.com>
2019-07-21 10:46:52 +0530

    non recursive solution

--- PRTAGN.py
+++ PRTAGN.py
@@ -1,61 +1,96 @@
+#
+# def countOnes(Q, i, ones):
+#
+#     Q, Q1 = logical_xor(Q, i)
+#     i[0] += 1
+#     E = ones[0]
+#     O = ones[1]
+#     # print(Q)
+#     # print(Q1)
+#     for each in Q1:
+#         if bin(each).count('1') % 2 == 0:
+#             E += 1
+#         else:
+#             O += 1
+#     ones = [E, O]
+#     if i[0] == n:
+#         print(ones[0], ones[1])
+#         return
+#     if i[0] == 0:
+#         print(ones[0], ones[1])
+#         countOnes(Q, i, ones)
+#     else:
+#         print(ones[0], ones[1])
+#         countOnes(Q, i, ones)
+#
+#
+# def logical_xor(Q, i):
+#
+#     x = int(input())
+#     # new = set()
+#     # new.add(x)
+#     new = set()
+#     new.add(x)
+#     if x not in Q:
+#         Q.add(x)
+#     else:
+#         return Q, set()
+#     if i[0] == 0:
+#         return Q, [x]
+#     for y in Q:
+#         if y != x:
+#             new.add(x ^ y)
+#     Q.update(new)
+#     # new.add(x)
+#     return Q, new
+#
+#
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     Q = set()
+#     i = [0]
+#     countOnes(Q, i, [0, 0])
+#     print('hey')
 
-def countOnes(Q, i, ones):
+    # print(counting[0], counting[1])
+    # print(calc)
 
-    Q, Q1 = logical_xor(Q, i)
-    E = ones[0]
-    O = ones[1]
-    print(Q)
-    print(Q1)
-    for each in Q1:
-        # if each not in calc:
-        # calc[each] = bin(each).count('1')
-        if bin(each).count('1') % 2 == 0:
-            E += 1
-        else:
-            O += 1
-        # else:
-        #     if calc[each] % 2 == 0:
-        #         E += 1
-        #     else:
-        #         O += 1
-    ones = [E, O]
-    if i == n-1:
-        print(ones[0], ones[1])
-        return
-    if i == 0:
-        print(ones[0], ones[1])
-        return countOnes(Q, i+1, ones)
-    print(ones[0], ones[1])
-    return countOnes(Q, i+1, ones)
+# 1
+# 3
+# 4
+# 2
+# 7
 
 
-def logical_xor(Q, i):
-
-    x = int(input())
-    # new = set()
-    # new.add(x)
-    new  = []
-    new.append(x)
-    if x not in Q:
-        Q.add(x)
-    else:
-        return Q, set()
-    if i == 0:
-        return Q, [x]
-    for y in Q:
-        if y != x:
-            new.append(x ^ y)
-    # Q.update(new)
-    # new.add(x)
-    return Q, new
 
 
 t = int(input())
 for _ in range(t):
     n = int(input())
     Q = set()
-    i = 0
-    countOnes(Q, i, [0, 0])
-    # print(counting[0], counting[1])
-    # print(calc)
+    e = 0
+    o = 0
+    for i in range(n):
+        x = int(input())
+        if x not in Q:
+            Q.add(x)
+        else:
+            print(e, o)
+            continue
+        new = set()
+        new.add(x)
+        for y in Q:
+            if y != x:
+                new.add(y ^ x)
+        # print(Q)
+        # print(new)
+        for each in new:
+            if bin(each).count('1') % 2 == 0:
+                e += 1
+            else:
+                o += 1
+        print(e, o)
+        Q.update(new)
+
 
