6015791fd444c3ff1dfeea7f0246a5f466b0df95
Mokshith Pyla <mokshithpyla@gmail.com>
2019-08-06 18:15:53 +0530

    factorial problem

---
+++ Dynamic_Programming/factorial.py
@@ -0,0 +1,11 @@
+def fact(N):
+    if N in memo:
+        return memo[N]
+    if N is 1 or N is 0:
+        return 1
+    else:
+        f = N * fact(N-1)
+    memo[N] = f
+    return f
+memo = {}
+print(fact(5))
