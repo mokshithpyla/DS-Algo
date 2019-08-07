c8332b23cb9c5a40ec880bba866703f4ac63796c
Mokshith Pyla <mokshithpyla@gmail.com>
2019-08-07 19:10:38 +0530

    codechef august challenge progress

--- Contests/KS1.py
+++ Contests/KS1.py
@@ -2,3 +2,14 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     A = list(map(int, input().split()))
+    i = 0
+    j = 1
+    k = 1
+    t = 0
+    for x in range(n-1):
+        lhs = A[i:j-1]
+        rhs = A[j:k]
+
+
+    print(t)
+
--- Contests/ZOMCAV.py
+++ Contests/ZOMCAV.py
@@ -1,84 +1,21 @@
-# t = int(input())
-# for _ in range(t):
-#     n = int(input())
-#     C = list(map(int, input().split()))
-#     H = list(map(int, input().split()))
-#     # radiation_levels = [0] * n
-#     memo = []
-#     for i in range(n):
-#         underflow = overflow = 0
-#         ones = 2 * C[i] + 1
-#         left_zeroes = 0
-#         right_zeroes = 0
-#         if C[i] > i:
-#             underflow = C[i] - i
-#         else:
-#             left_zeroes = i - C[i]
-#         if C[i] > n - i - 1:
-#             overflow = C[i] - (n - i - 1)
-#         else:
-#             right_zeroes = (n - i - 1) - C[i]
-#         ones = ones - (underflow + overflow)
-#         # temp = [0] * left_zeroes + [1] * ones + [0] * right_zeroes
-#         temp = [left_zeroes, ones, right_zeroes]
-#         memo.append(temp)
-#         # for j in range(n):
-#         #     radiation_levels[j] += temp[j]
-#     x = [0] * n
-#     for each in memo:
-#         temp = [0] * each[0] + [1] * each[1] + [0] * each[2]
-#
-#     radiation_levels = [sum(i) for i in zip(*memo)]
-#     # print(radiation_levels)
-#     radiation_levels.sort()
-#     H.sort()
-#     if H == radiation_levels:
-#         print('YES')
-#     else:
-#         print('NO')
-
-# 2
-# 5
-# 1 2 3 4 5
-# 1 2 3 4 5
-# 5
-# 5 4 3 2 1
-# 1 2 3 4 5
-from collections import defaultdict
 t = int(input())
 for _ in range(t):
     n = int(input())
     C = list(map(int, input().split()))
     H = list(map(int, input().split()))
-    # radiation_levels = [0] * n
-    memo = []
-    radiation_power = defaultdict
+    total_zeroes = 0
     for i in range(n):
-        underflow = overflow = 0
-        ones = 2 * C[i] + 1
         left_zeroes = 0
         right_zeroes = 0
-        if C[i] > i:
-            underflow = C[i] - i
-        else:
+        if C[i] <= i:
             left_zeroes = i - C[i]
-        if C[i] > n - i - 1:
-            overflow = C[i] - (n - i - 1)
-        else:
+        if C[i] <= n - i - 1:
             right_zeroes = (n - i - 1) - C[i]
-        ones = ones - (underflow + overflow)
-        radiation_power[i] += 1
-        # for j in range(n):
-        #     radiation_levels[j] += temp[j]
-    x = [0] * n
-    for each in memo:
-        temp = [0] * each[0] + [1] * each[1] + [0] * each[2]
-
-    radiation_levels = [sum(i) for i in zip(*memo)]
-    # print(radiation_levels)
-    radiation_levels.sort()
-    H.sort()
-    if H == radiation_levels:
+        total_zeroes += left_zeroes + right_zeroes
+    max_sum = n*n
+    c1 = max_sum - total_zeroes
+    c2 = sum(H)
+    if c1 == c2:
         print('YES')
     else:
         print('NO')
